3097f93d0810a6ecb473bf6b9122f28830da730b
Paul Renoult <prenoult@me.com>
2024-12-03 23:54:52 +0100

    feat: add day 3 part 2

--- Day 3/main.py
+++ Day 3/main.py
@@ -15,3 +15,27 @@ print(
   "Part 1:",
   sum(get_product_from_multiplication_tuples(get_multiplication_tuples(content)))
 ) 
+
+# part 2
+def get_eligible_multiplication_tuples(text):
+  pattern = r"(do\(\)|don't\(\))|mul\((\d+),(\d+)\)"
+  enabled = True
+  eligible_tuples = []
+
+  for match in re.finditer(pattern, text):
+    command = match.group(0)
+
+    if command == "do()":
+      enabled = True
+    elif command == "don't()":
+      enabled = False
+    elif enabled and command.startswith("mul"):
+      x,y = int(match.group(2)), int(match.group(3))
+      eligible_tuples.append((x,y))
+
+  return eligible_tuples
+
+print(
+  "Part 2:",
+  sum(get_product_from_multiplication_tuples(get_eligible_multiplication_tuples(content)))
+)
